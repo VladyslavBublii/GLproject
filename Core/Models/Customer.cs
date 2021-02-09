a78d6e050232c506ef92e04bb8f84ddcf7a2fecf
Vladyslav Bublii <53186169+VladyslavBublii@users.noreply.github.com>
2021-02-09 11:50:31 +0200

    Customer
    add Customer model

---
+++ Core/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Models
+{
+    class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public string City { get; set; }
+
+        public string PostIndex { get; set; }
+    }
+}
--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -3,8 +3,11 @@
     public class User
     {
         public int Id { set; get; }
+
         public int Role { set; get; }
+
         public string Email { set; get; }
+
         public string Password { set; get; }
     }
 }
