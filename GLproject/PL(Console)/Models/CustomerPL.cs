260057152f67da7508b947bd034cf2b5acd04a04
Vladyslav Bublii <53186169+VladyslavBublii@users.noreply.github.com>
2021-02-12 01:53:52 +0200

    Regis in PL
    Regis in PL

---
+++ GLproject/PL(Console)/Models/CustomerPL.cs
@@ -0,0 +1,14 @@
+﻿
+namespace PL_Console_.Models
+{
+    public class CustomerPL
+    {
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public string City { get; set; }
+
+        public string PostIndex { get; set; }
+    }
+}
---
+++ GLproject/PL(Console)/Models/UserPL.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PL_Console_.Models
+{
+    public class UserPL
+    {
+        public string Email { set; get; }
+
+        public string Password { set; get; }
+    }
+}
