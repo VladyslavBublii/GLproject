b36e74fb99ab01b378f0c0ad309ecc860433bdfc
TruaAM <61706825+TruaAM@users.noreply.github.com>
2023-05-31 17:15:33 +0200

    Add reg and log

--- PL.Angular/ClientApp/src/app/app.module.ts
+++ PL.Angular/ClientApp/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { NavFooterComponent } from './nav-footer/nav-footer.component';
 import { HomeComponent } from './home/home.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
@@ -17,6 +20,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     NavFooterComponent,
     HomeComponent,
+    PrivacyComponent,
+    RegisterComponent,
     CounterComponent,
     FetchDataComponent
   ],
@@ -26,6 +31,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'privacy', component: PrivacyComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
