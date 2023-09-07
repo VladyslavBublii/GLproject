58b7f6af17e3f440975adc0a0a40b006323c6bfd
TruaAM <61706825+TruaAM@users.noreply.github.com>
2023-09-07 11:06:38 +0200

    Add http post for login controller

---
+++ PL.Angular/ClientApp/src/app/login/login.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+
+  private HttpOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {}
+
+  signin(){
+    return this.http.get(this.baseUrl + "login/signin", this.HttpOptions);
+  }
+
+  signinto(data: any) {
+    console.log(data);
+    console.log(JSON.stringify(data));
+    return this.http.post(this.baseUrl + "login/signin", JSON.stringify(data), this.HttpOptions);
+  }
+}
