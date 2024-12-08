23577f18efbb9b7764873633827e55e00855faa5
TruaAM <61706825+TruaAM@users.noreply.github.com>
2024-12-08 20:46:08 +0100

    FIX Errors with constructs

---
+++ BL/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using BL.DTO;
+using Core.Models;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<Product, ProductDTO>().ReverseMap();
+        CreateMap<Cart, CartDTO>().ReverseMap();
+    }
+}
