ac3572aec9e61b4dd09f87787a4472336b4d4e14
TruaAM <61706825+TruaAM@users.noreply.github.com>
2025-01-07 20:13:07 +0100

    Fix display products in shop

--- BL/Services/MainProductInformationService.cs
+++ BL/Services/MainProductInformationService.cs
@@ -5,6 +5,7 @@ using Core.Models;
 using DAL.Interfaces;
 using DAL.Repositories;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BL.Services
 {
@@ -17,10 +18,13 @@ namespace BL.Services
             _unitOfWork = new UnitOfWork();
         }
 
-        public IEnumerable<MainProductInformationDTO> GetProductsAsync()
+        public async Task<IEnumerable<MainProductInformationDTO>> GetProductsAsync()
         {
+            var products = await _unitOfWork.Products.GetAllAsync();
+
             var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Product, MainProductInformationDTO>()).CreateMapper();
-            return mapper.Map<IEnumerable<Product>, List<MainProductInformationDTO>>((IEnumerable<Product>)_unitOfWork.Products.GetAllAsync());
+
+            return mapper.Map<IEnumerable<Product>, List<MainProductInformationDTO>>(products);
         }
     }
 }
